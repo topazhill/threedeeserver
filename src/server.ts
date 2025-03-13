0a62a4679c1bf8eca4a4fd6e9922728fa87457ad
topazhill <thill@perse.co.uk>
2025-03-13 17:22:56 +0000

    add files and setup

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import * as express from 'express';
+import * as path from 'path';
+
+const clients: { [id:string]: any } = {};
+
+const port: number = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    clients[socket.id] = {};
+    socket.emit('id', socket.id);
+
+    socket.on('disconnect', () => {
+        if (clients && clients[socket.id]) {
+            delete clients[socket.id];
+            io.emit('removeClient', socket.id);
+            console.log('a user disconnected');
+        }
+    });
+
+    socket.on('update', (data) => {
+        if (clients[socket.id]) {
+            clients[socket.id] = data;
+        }
+    });
+});
+
+setInterval(() => {
+    io.emit('clients', clients);
+}, 1000 / 60);
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
